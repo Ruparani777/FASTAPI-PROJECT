0b3723d21f8f35108eb3cbf064e77d395aa3ed3a
Ruparani777 <96835039+Ruparani777@users.noreply.github.com>
2025-05-16 06:47:52 +0530

    Create crud.py

---
+++ app/crud.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_note(db: Session, note_id: int):
+    return db.query(models.Note).filter(models.Note.id == note_id).first()
+
+def get_notes(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Note).offset(skip).limit(limit).all()
+
+def create_note(db: Session, note: schemas.NoteCreate):
+    db_note = models.Note(title=note.title, content=note.content)
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return db_note
+
+def delete_note(db: Session, note_id: int):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if note:
+        db.delete(note)
+        db.commit()
+        return True
+    return False
